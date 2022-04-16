0b243bf1f72249f2206d82462253e25af6a70f9c
TelmaJ <tej2@hi.is>
2022-04-16 21:19:41 +0000

    finish síða uppsett

---
+++ src/main/java/com/example/tripplanner/Controllers/FinishCardController.java
@@ -0,0 +1,6 @@
+package com.example.tripplanner.Controllers;
+
+import javafx.scene.layout.Pane;
+
+public class FinishCardController extends Pane {
+}
--- src/main/java/com/example/tripplanner/Controllers/FinishController.java
+++ src/main/java/com/example/tripplanner/Controllers/FinishController.java
@@ -1,4 +1,13 @@
 package com.example.tripplanner.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
 public class FinishController {
+
+    @FXML
+    private VBox fxPassengerInfoCont;
+    @FXML
+    private Pane PaymentCont;
 }
